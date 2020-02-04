489cb37a66483726d3a8f0609f9a5b1cfee2d7a7
Cameron <noremackientz@gmail.com>
2020-02-04 16:39:18 -0600

    fixed .gitignore

--- target/debug/acheron.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Dev/Rust/acheron/target/debug/acheron: /home/ubuntu/Dev/Rust/acheron/src/listener.rs /home/ubuntu/Dev/Rust/acheron/src/main.rs /home/ubuntu/Dev/Rust/acheron/src/sender.rs
